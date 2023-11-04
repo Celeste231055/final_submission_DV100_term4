61adbc27a4ab60d789cd88c44afc10ce2e7e1a1c
LindaNgomane <125291929+LindaNgomane@users.noreply.github.com>
2023-11-04 20:50:56 +0200

    Second card integration

--- scripts/main.js
+++ scripts/main.js
@@ -165,81 +165,130 @@ function  displaySliderMovies(movies) {
 function allComedyMovies(genre){
 
    
-    // Currently we are getting movies for adult=false, video=false, language=en-US, page=1, sort_by=popularity.desc, with_genres=35
-    // To add a parameter check out this link https://developer.themoviedb.org/reference/discover-movie
-    const apiUrl = `https://api.themoviedb.org/3/discover/movie?api_key=a6ca981513c9c7f4fc02008ff4ad8402&include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&with_genres=${genre}`;
-    
-    $.ajax({
-        url: apiUrl,
-        method: 'GET',
-        dataType: 'json',
-        success: function(data){
-
+    //  Api url
+      const apiUrl = `https://api.themoviedb.org/3/discover/movie?api_key=a6ca981513c9c7f4fc02008ff4ad8402&include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&with_genres=${genre}`;
+      
+      $.ajax({
+          url: apiUrl,
+          method: 'GET',
+          dataType: 'json',
+          success: function(data){
             //map the api
-            const allMovies = data.results.map(movie => ({
-                id: movie.id,
-                title: movie.title,
-                image: movie.poster_path,
-                description: movie.overview,
-                release: movie.release_date,
-            }))
-
-            displayMovies(allMovies);
-            console.log(data);
-        },
-        error: function(data){}
-            
-            
-    });
-};
-
-// Here we will display the movies
-function displayMovies(allMovies){
-
-  // We will append the card to the movie container later
-    const libraryContainer = $("#movie-cards");
-    libraryContainer.empty();
-    
-    //Loop though the movies.
-    allMovies.forEach(movie => {
-        
-        const card = $(`
-        <div class="col-12 col-md-3 mt-5 justify-content-center"> <!-- Adjust the column size as needed -->
-          <!-- The Card -->
-          <div class="card" value="${movie.id}">
-            <!-- Img goes here -->
-            <img src="https://image.tmdb.org/t/p/original${movie.image}" class="poster rounded-1" alt="${movie.title}">
-            <!-- Card Body -->
-            <div class="details">
-              <div class="row">
-                <div class="col-10"><h5 class="title">${movie.title}</h5></div>
-                <div class="col-2"><i class="bi bi-plus-circle" onclick="addToWatchlist(${movie.id})"></i></div>
+            const allMovies = data.results.map((movie) => ({
+              id: movie.id,
+              title: movie.title,
+              image: movie.poster_path,
+              description: movie.overview,
+              rating: movie.vote_average,
+              genre1: movie.genre_ids[0],
+              genre2: movie.genre_ids[1],
+              genre3: movie.genre_ids[2]
+            }));
+  
+              
+  
+              //load cards when successful 
+              displayMovies(allMovies);
+              console.log(data);
+          },
+          error: function(data){}
+              
+              
+      });
+  };
+  
+  // Display the movie cards
+  function displayMovies(allMovies){
+  
+    // Create a variable for the movie Container
+      const movieContainer = $('#movie-cards');
+      movieContainer.empty();
+      
+      //Loop though the movies.
+      allMovies.forEach(movie => {
+          
+          const card = $(`   
+          <div class="col-12 col-md-6 col-lg-4 col-xxl-3 mt-5 d-flex justify-content-center">
+  
+              <!-- The Card -->
+              <div class="card" value="${movie.id}">
+  
+                <!-- Img goes here -->
+                  <img src="https://image.tmdb.org/t/p/original${movie.image}" class="poster rounded-1" alt="${movie.title}">
+                  
+                  <!-- Card Body -->
+                  <div class="details">
+  
+                    <!-- Float title to the left and icon to the right. Here you can change the icon from a minus to a plus. Remember to do the same for css-->
+                    <!-- ---------------------------------------------------------------------------------------------------------------------------------- -->
+                    <div class="row">
+                      <div class="col-10"><h5 class="title">${movie.title}</h5></div>
+                      <div class="col-2"><i class="bi bi-plus-circle" onclick="addToWatchlist(${movie.id})"></i></div>
+                    </div>
+  
+                    <!--Rating-->
+                    <p style="color: white;" class="pf-3">Rating: ${movie.rating} <i class="bi bi-star-fill"></i></p>
+                    <div class="genres">
+                      <span class="genre pf-4"><b>${movie.genre1}</b></span>
+                      <span class="genre pf-4"><b>${movie.genre2}</b></span>
+                      <span class="genre pf-4"><b>${movie.genre3}</b></span>
+                    </div>
+                    <br>
+                    <!--Description-->
+                    <div class="fadeout"><p class="card-text pf-3">${movie.description}</p></div>
+                    <!-- More Info button -->
+                      <button type="button" class="button btn-default">More Info</button>
+  
+                  </div>
               </div>
-              <p style="color: white;" class="pf-3">Directed by Director</p>
-              <p style="color: white;" class="pf-3">1h 44m</p>
-              <div class="genres">
-                <span class="genre pf-4"><b>Comedy</b></span>
-                <span class="genre pf-4"><b>Sitcom</b></span>
-                <span class="genre pf-4"><b>Mockumentary</b></span>
-              </div>
-              <br>
-              <div class="fadeout"><p class="card-text pf-3">${movie.description}</p></div>
-              <!-- More Info button -->
-              <button type="button" class="button btn-sm more-info">More Info</button>
-            </div>
           </div>
-        </div>
-      `)       
-        
-        // Take User to the Individual Movie Page when clicking on the More Info Button
-        card.on('click','.more-info',function(){
-          window.location.href =`http://127.0.0.1:5501/pages/individual.html?id=${movie.id}`;
-
-        })
-        
-        // Here we append the card to the container.
-        libraryContainer.append(card);
-
+          `)       
+          
+          //Genres
+          const genres = genreNames = ([movie.genre1, movie.genre2, movie.genre3]);
+          const genreSpans = genres.map(genre => `<span class="genre pf-4"><b>${genre}</b></span>`).join('');
+          card.find('.genres').html(genreSpans);
+          
+          // Take User to the Individual Movie Page when clicking on the More Info Button
+          card.on('click','.btn-default',function(){
+            window.location.href =`http://127.0.0.1:5501/pages/individual.html?id=${movie.id}`;
+  
+          })
+          
+          // Append the card to the container.
+          movieContainer.append(card);
+  
+          //when btn is clicked change icon class/display check icon
+          $(card).find(".bi-plus-circle").click(function(){
+            $(this).attr('class', 'bi bi-check-circle');
             
-    });
-}
+          });
+              
+      });
+  }
+  
+  //create function that will be called when plus btn is clicked
+  function addToWatchlist(movieId) {
+  
+    
+    // get watchlist info from local storage or create an empty array. We want to store everything in one array not hundreds of them.
+    let watchlist = JSON.parse(localStorage.getItem('watchlistMovies')) || [];
+  
+    
+    // If watchlist is !not an array then make it one. You shall not pass unless you are an array!
+    if (!Array.isArray(watchlist)) {
+        watchlist = [];
+    }
+  
+    // If this movie id is not already in the watchlist then push it to watchlist array. Aka no doubles
+    if (!watchlist.includes(movieId)) {
+    
+    // Add movieId to watchlist
+    watchlist.push(movieId);
+    }
+  
+    // Save again to local storage
+    localStorage.setItem('watchlistMovies', JSON.stringify(watchlist));
+    console.log(watchlist);
+  };
+  
